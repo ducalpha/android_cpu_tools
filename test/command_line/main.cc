7eaf1bed2fde3eb9e5b4d815231fd75c5248f9df
Duc Bui <ducbuihoang@gmail.com>
2016-04-27 01:12:14 +0900

    add another command line test

--- test/command_line/main.cc
+++ test/command_line/main.cc
@@ -5,9 +5,16 @@
 
 #include "base/command_line.h"
 #include "base/logging.h"
+#include "base/strings/string_split.h"
 
 int main(int argc, char **argv) {
   base::CommandLine cmdline(argc, argv);
-  LOG(INFO) << cmdline.GetSwitchValueASCII("cluster-info");
+
+  std::string args = " --cpu-core-ids=0-7 --cluster-core-ids=0-3,4-7 --cluster-freqs=500000-1300000,800000-1900000 --cluster-freq-governors=interactive,interactive";
+  std::vector<std::string> another_argv = base::SplitString(args, " ", base::TRIM_WHITESPACE, base::SPLIT_WANT_NONEMPTY);
+
+  cmdline.AppendArguments(base::CommandLine(another_argv), false);
+
+  LOG(INFO) << "Combined command line: " << cmdline.GetCommandLineString();
   return 0;
 }
